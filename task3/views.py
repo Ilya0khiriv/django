f35b3abb21a5812bb7e723b65574f3f34a3c32ae
Ilya <ilya@Ilyas-MacBook-Air.local>
2024-11-09 19:31:01 +0500

    Initial commit

---
+++ task3/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from task3.lang_platform import all_langs as localization
+print(localization)
+
+chosen_lang = "Русский"
+context = dict(localization[chosen_lang].__dict__)
+
+def platform(request):
+    return render(request, 'third_task/platform.html', context)
+
+def games(request):
+    return render(request, 'third_task/store.html', context)
+
+def cart(request):
+    return render(request, 'third_task/cart.html', context)
+
+def lang(request):
+    global chosen_lang
+    global context
+    list_lang = list(localization.keys())
+
+    if request.method == 'GET':
+        lang = request.GET.get('lang', '')
+        if lang in list_lang:
+            print(lang, "hoooooray")
+
+            chosen_lang = lang
+            context = dict(localization[chosen_lang].__dict__)
+
+    context.update({"universal_langs": list_lang})
+
+    return render(request, 'third_task/lang.html', context)
